c465d975ebb6def843bd85e143320dda82ac440d
HappyS121 <happylittlesteps1@gmail.com>
2025-08-20 17:02:35 +0300

    Fix corrupted App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to ContaAI</h1>
+      {/* Your application content will go here */}
+    </div>
+  );
+}
+
+export default App;
